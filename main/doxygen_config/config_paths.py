37eb3eef64215edc95ecb42e41adc648b61a7aa5
Ismail Al Shuaybi <ismail.alshuaybi@student.htw-berlin.de>
2023-06-26 21:32:02 +0200

    feat: Add config folder for Doxyfile paths and variables
    This commit introduces a new folder named 'config' that contains two
    python files - 'config_paths.py' and 'config_variables.py'. These files
    provide a centralized location for managing various paths and variables
    used in the Doxyfile configuration process. This approach enhances
    maintainability and simplifies the configuration process.

---
+++ main/doxygen_config/config_paths.py
@@ -0,0 +1,6 @@
+# Path to the Doxyfile in the doxygen configuration directory
+DOXYFILE_PATH = "doxygen_configuration/Doxyfile"
+# Path to the doxygen configuration directory
+DOXYFILE_DIRECTORY_PATH = "doxygen_configuration/"
+# Path to the directory containing the doxygen projects
+PROJECT_DIRECTORY_PATH = "doxygen_projects"
