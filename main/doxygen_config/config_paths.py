491dce15a2fbd29fd15cf6b83ae0f89bfebec806
Ismail Al Shuaybi <ismail.alshuaybi@student.htw-berlin.de>
2023-08-05 00:12:21 +0200

    feat: Remove unnecessary files

--- main/doxygen_config/config_paths.py
+++
@@ -1,6 +0,0 @@
-# Path to the Doxyfile in the doxygen configuration directory
-DOXYFILE_PATH = "doxygen_configuration/Doxyfile"
-# Path to the doxygen configuration directory
-DOXYFILE_DIRECTORY_PATH = "doxygen_configuration/"
-# Path to the directory containing the doxygen projects
-PROJECT_DIRECTORY_PATH = "doxygen_projects"
